5065e701fd429d427f224b194db2a7d780bd708b
ntDat <cudeanhno@gmail.com>
2024-05-29 17:26:14 +0700

    update

---
+++ app/src/main/java/com/example/foodappkotlin/activites/PlaceYourOrderActivity.kt
@@ -0,0 +1,11 @@
+package com.example.foodappkotlin.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlaceYourOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_place_your_order)
+    }
+}
---
+++ app/src/main/java/com/example/foodappkotlin/adapter/PlaceYourOrderAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.foodappkotlin.adapter
+
+class PlaceYourOrderAdapter {
+}
